f5c1f63ddf7873dd848d33c21434edb1c7049f97
GrandestJax <491659@student.roundrockisd.org>
2014-10-22 15:56:19 -0500

    Done

--- review.py
+++ review.py
@@ -18,3 +18,14 @@
 # Remember how range can be used to create the list [2,4,6,8]
 # When complete, commit to github and submit a pull request!
 
+lis = [2,4,6,8]
+song = 2
+
+
+while song > -1:
+    print "Who do we appreciate?"
+    song = song - 1
+    for x in lis:
+        print x
+    if song == -1:
+        print "Mr.edgeley"
